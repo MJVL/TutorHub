f474e7ac13a948468e623a2e6f73313922986792
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-04 17:02:47 -0400

    Add MainGUI class

---
+++ TutorHub/src/MainGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MainGUI {
+
+    protected JPanel MainPanel;
+    protected JButton btnStudent;
+    protected JButton btnTutor;
+    private JLabel lblTitle;
+
+    public MainGUI() {
+        MainPanel.requestFocus();
+    }
+
+}
