f252bf8087d991c043837b9a3f3679e36333e445
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-04 19:19:46 -0400

    Edit LandingWindow

--- TutorHub/src/LandingWindow.java
+++ TutorHub/src/LandingWindow.java
@@ -4,4 +4,7 @@ public class LandingWindow {
     protected JPanel MainPanel;
     private JButton logInButton;
     private JButton signUpButton;
+    private JButton btnLogin;
+    private JButton btnSignup;
+    private JLabel lblTitle;
 }
