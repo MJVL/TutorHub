08d4bc7d9ef3268580c4d543b9e4a99e828a9ae7
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-04 17:04:07 -0400

    Add SubjectGUI class

---
+++ TutorHub/src/SubjectGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class SubjectGUI {
+    protected JPanel MainGUI;
+    private JButton btnMath;
+    private JLabel lblTitle;
+    private JButton btnScience;
+    private JButton btnSocialStudies;
+    private JButton btnForeignLanguages;
+    private JButton btnTesting;
+    private JButton btnOther;
+    private JButton btnProgramming;
+    private JButton btnLanguageArts;
+}
