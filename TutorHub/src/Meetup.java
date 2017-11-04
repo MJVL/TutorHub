b5b0a9c5771880459f1e939830ec767c3155334c
thearkochatterjee <arkochatterjee1006@gmail.com>
2017-11-04 18:47:27 -0400

    Add Meetup class

---
+++ TutorHub/src/Meetup.java
@@ -0,0 +1,52 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Meetup {
+    private String type, location;
+    private ArrayList<Students> arrstudents = new ArrayList<Students>();
+    private ArrayList<Tutors> arrtutors = new ArrayList<Tutors>();
+
+    public Meetup()
+    {
+        type = "";
+        location  = "";
+        arrstudents = new ArrayList<Students>();
+        arrtutors = new ArrayList<Tutors>();
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public ArrayList<Students> getArrstudents() {
+        return arrstudents;
+    }
+
+    public ArrayList<Tutors> getArrtutors() {
+        return arrtutors;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+
+    public void setArrstudents(ArrayList<Students> arrstudents)
+    {
+        this.arrstudents = arrstudents;
+    }
+
+    public void setArrtutors(ArrayList<Tutors> arrtutors)
+    {
+        this.arrtutors = arrtutors;
+    }
+}
