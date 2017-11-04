9dbadfe9186fc2edec02a2a51e4214ceeede68df
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-04 16:47:53 -0400

    Add methods

--- TutorHub/src/Question.java
+++ TutorHub/src/Question.java
@@ -5,10 +5,37 @@ public class Question {
     String myQuestion;
     ImageIcon myImage;
 
-    public Question (String myQuestion, ImageIcon myImage) {
+    public Question() {
+        myQuestion = null;
+        myImage = null;
+    }
+
+    public Question(String myQuestion) {
+        this.myQuestion = myQuestion;
+    }
+
+    public Question(String myQuestion, ImageIcon myImage) {
         this.myQuestion = myQuestion;
         this.myImage = myImage;
     }
+
+    public void setQuestion(String myQuestion) {
+        this.myQuestion = myQuestion;
+    }
+
+    public String getQuestion() {
+        return myQuestion;
+    }
+
+    public void setMyImage(ImageIcon myImage) {
+        this.myImage = myImage;
+    }
+
+    public String toString() {
+        return m
+    }
+
+
 
 
 }
