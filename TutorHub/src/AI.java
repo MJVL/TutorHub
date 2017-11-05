3f374c9629f83a2d63c45378bcd7c5ef0ade6a35
thearkochatterjee <arkochatterjee1006@gmail.com>
2017-11-05 00:01:51 -0400

    Add AI class to find similar question

---
+++ TutorHub/src/AI.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class AI {
+    private ArrayList<Question> arrallquestions = new ArrayList<Question>();
+
+    public AI(){
+        arrallquestions = new ArrayList<Question>();
+    }
+
+    public AI(ArrayList<Question> arrallquestions){
+        this.arrallquestions = arrallquestions;
+    }
+
+    public ArrayList<Question> getArrallquestions() {
+        return arrallquestions;
+    }
+
+    public void setArrallquestions(ArrayList<Question> arrallquestions) {
+        this.arrallquestions = arrallquestions;
+    }
+
+    public Question findsimilarquestion(Question find){
+        String arrwords[] = find.getQuestion().split(" ");
+        int count = 0;
+        double pmatch = 0.0;
+        Question result = new Question();
+        int pos = 0;
+        for(int x = 0;x<arrwords.length;x++){
+            String arrcomwords[] = arrallquestions.get(x).getQuestion().split(" ");
+            count = 0;
+            for(int y = 0;y<arrcomwords.length;y++){
+                if(arrwords[x].equals(arrcomwords[y])){
+                    count++;
+                }
+            }
+            if((count/arrcomwords.length)>pmatch){
+                pmatch = count / arrcomwords.length;
+                pos = x;
+            }
+        }
+        result = arrallquestions.get(pos);
+        return result;
+    }
+}
