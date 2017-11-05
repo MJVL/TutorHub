d0d324e202055be95ffac6a99b465589c046a0fd
Fiona Xie <fifix2001@gmail.com>
2017-11-04 21:48:06 -0400

    made MathWindow

---
+++ TutorHub/src/MathWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MathWindow {
+    protected JButton btnAlgebra1;
+    protected JButton btnGeometry;
+    protected JButton btnAlgebra2;
+    protected JButton btnCalculus;
+    protected JButton btnPreC;
+    protected JButton btnStatistics;
+    protected JButton btnAQR;
+    protected JButton btnOther;
+    protected JPanel MainPanel;
+    protected boolean Visible;
+}
