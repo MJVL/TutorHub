4ba06882ef5300b4187677f7901357013d564841
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-05 12:08:45 -0500

    Edit window

--- TutorHub/src/TutorWindow.java
+++ TutorHub/src/TutorWindow.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class TutorWindow {
     protected JPanel MainPanel;
     protected JList lstQuestions;
-    protected JList lstEvents;
     protected JList lstSubjects;
     protected JComboBox cboxAddSubject;
     protected boolean Visible;
